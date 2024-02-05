0735a204abcbce698e786ccf290bbd02f2ed5547
Didi Watson <mdodson55@gmail.com>
2024-02-05 11:58:32 -0700

    update ingredient model

--- models/ingredient_model.py
+++ models/ingredient_model.py
@@ -4,7 +4,7 @@ from models import db
 
 class Ingredient(db.Model):
     __tablename__= "ingredients"
-    id = db.Column(db.String(100), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     category = db.Column(db.String(100))
     dateAdded = db.Column(db.DateTime, server_default=func.now())
