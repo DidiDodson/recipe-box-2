4334b6ead7386ecffc0d81d9af09e80399921594
Didi Watson <mdodson55@gmail.com>
2024-02-06 08:05:57 -0700

    add routes

--- api_routes.py
+++ api_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, current_app, send_from_directory, render_template
+from flask import Blueprint, current_app, send_from_directory, render_template, make_response, jsonify, request
+import json
 
 from models import db
 
@@ -9,7 +10,43 @@ app = current_app
 api = Blueprint('api', __name__)
     
 @api.route("/")
-def index():
-    recipes = Recipe.query.all()
+def homepage():
 
-    return render_template('hello.html', recipes=recipes)    
+    return render_template('hello.html') 
+
+@api.route('recipes', methods=['GET'])
+def get_recipes():
+    try: 
+        recipes = Recipe.query.order_by(Recipe.name).all()
+
+        return make_response(jsonify([recipe.json() for recipe in recipes]), 200)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error getting recipes'}))    
+
+@api.route('recipes/<id>', methods=['GET'])
+def get_recipe(id):
+    try: 
+        recipe = Recipe.query.filter_by(id=id).first()
+
+        return make_response(jsonify({'recipe': recipe.json()}), 200)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error getting recipe'}))
+    
+@api.route('ingredients', methods=['GET'])
+def get_ingredients():
+    try: 
+        ingredients = Ingredient.query.order_by(Ingredient.name).all()
+
+        return make_response(jsonify([ingredient.json() for ingredient in ingredients]), 200)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error getting ingredients'}))
+    
+@api.route('ingredients/<id>', methods=['GET'])
+def get_ingredient(id):
+    try: 
+        recipe = Recipe.query.filter_by(id=id).first()
+        print("R", recipe)
+
+        return make_response(jsonify({'recipe': recipe.json()}), 200)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error getting recipe'}))    
