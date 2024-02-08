978e1cc53a40765953dd2344321dda1d37aca137
Didi Watson <mdodson55@gmail.com>
2024-02-08 13:49:41 -0700

    Updates to app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,54 +3,61 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
+import { Ingredients } from "./ingredients";
+
 export default function ButtonAppBar() {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
+
     const handleClick = (event) => {
       setAnchorEl(event.currentTarget);
     };
+
     const handleClose = () => {
       setAnchorEl(null);
     };
-  
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar style={{ background: '#2E3B55' }}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={handleClick}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-            'aria-labelledby': 'basic-button',
-            }}
-            >
-            <MenuItem onClick={handleClose}>Recipes</MenuItem>
-            <MenuItem onClick={handleClose}>Ingredients</MenuItem>
-            </Menu>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Recipe Box
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar style={{ background: '#2E3B55' }}>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                    onClick={handleClick}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                    }}
+                    >
+                    <MenuItem onClick={handleClose} >
+                        <Link href="/" underline="none" color="black">Home Page</Link>
+                    </MenuItem>    
+                    <MenuItem onClick={handleClose} >
+                        <Link  href="/ingredients" underline="none" color="black">Ingredients</Link>
+                    </MenuItem>
+                </Menu>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Recipe Box
+                </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
